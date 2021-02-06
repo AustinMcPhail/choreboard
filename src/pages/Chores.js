d9c223ae1f7e123ef1b02bcdadc0aafe948f211e
Austin McPhail <austin@mcphail.dev>
2021-02-06 08:47:41 -0600

    feat: 🎸 routing (#5)

---
+++ src/pages/Chores.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Chores = () => (
+  <div>
+    <h2>Chores</h2>
+  </div>
+)
+
+export default Chores
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+// Todo: A home component that:
+// 1. Shows the top 3 highlighted bulletin board flyers
+// 2. Shows a swipable list of jobs
+// 3. Swipe right adds to "interested list", swipe left adds to "not interested list".
+import React from 'react'
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+export default Home
