e2da5b88eb40e3eb5ae7d3223c75ca28813f011f
drake <tehtrojenguy@gmail.com>
2021-02-06 15:29:23 -0600

    Created poster board

--- src/components/PosterBoard.js
+++ src/components/PosterBoard.js
@@ -7,9 +7,43 @@ function PosterBoard() {
   const [leftImg, setLeftImg] = useState(
     'https://s3.amazonaws.com/thumbnails.venngage.com/template/f5e28890-6c0a-47f5-bc08-1e842df9f988.png'
   )
+  const [posterList, setPosterList] = useState([
+    'https://canfar.com/wp-content/uploads/2018/02/En-Needles.jpg',
+    'https://s3.amazonaws.com/thumbnails.venngage.com/template/f5e28890-6c0a-47f5-bc08-1e842df9f988.png',
+  ])
+  const [posterIndex, setPosterIndex] = useState(0)
+
+  const flipPosterLeft = () => {
+    console.log('Going Left!')
+    if (posterIndex === 0) {
+      setPosterIndex(posterList.length - 1)
+    } else {
+      setPosterIndex(posterIndex - 1)
+    }
+  }
+
+  const flipPosterRight = () => {
+    console.log('Going Right!')
+    if (posterIndex === posterList.length - 1) {
+      setPosterIndex(0)
+    } else {
+      setPosterIndex(posterIndex + 1)
+    }
+  }
+
   return (
-    <div className="flex justify-center">
-      <img src={frontImg} alt="" className="w-1/2 h-64" />
+    <div className="flex justify-between">
+      <div className="my-auto w-1/4 flex justify-center">
+        <button onClick={flipPosterLeft} type="button" className="w-1/2">
+          {'<'}
+        </button>
+      </div>
+      <img src={posterList[posterIndex]} alt="" className="w-1/2 h-64" />
+      <div className="my-auto w-1/4 flex justify-center">
+        <button onClick={flipPosterRight} type="button" className="w-1/2">
+          {'>'}
+        </button>
+      </div>
     </div>
   )
 }
