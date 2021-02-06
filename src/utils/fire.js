17857aeedfb9d496e6e179a9254425ceced2011f
Austin McPhail <austin@mcphail.dev>
2021-02-06 11:47:46 -0600

    feat: 🎸 database

---
+++ src/utils/fire.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/firestore'
+
+const config = {
+  apiKey: 'AIzaSyA4q8PR7maTIJlNsg0Di8OI9V4eYYa1MTc',
+  authDomain: 'choreboard-32541.firebaseapp.com',
+  databaseURL: 'https://choreboard-32541-default-rtdb.firebaseio.com',
+  projectId: 'choreboard-32541',
+  storageBucket: 'choreboard-32541.appspot.com',
+  messagingSenderId: '290120382749',
+  appId: '1:290120382749:web:6196030ccbeca6446bc9f4',
+}
+
+const fire = firebase.initializeApp(config)
+
+export const writeChore = (chore) => {
+  fire.database().ref('chores').push({
+    desc: 'description',
+    expiry: new Date().toISOString(),
+  })
+}
+
+export default fire
